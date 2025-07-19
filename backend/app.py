4f64cdedf7af7d3e1bb3e4dee2e6ac3ec6beb181
flowcore-aidev <w63towing@gmail.com>
2025-07-19 02:57:23 -0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def home():
     return "Lydell Tech is live!"
 
-    if __name__ == "__main__":
-app.run(host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
